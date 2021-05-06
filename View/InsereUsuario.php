93d198135096409bb9b751df426d639f40fa2be0
Guzz-drk <182740@upf.br>
2021-05-06 20:55:14 -0300

    Form

--- View/InsereUsuario.php
+++ View/InsereUsuario.php
@@ -6,22 +6,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
    <?php include "menu.php" ?>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-    <form action="../Controller/controller.php" method="POST">
-    <label for="">Nome</label>
-    <input type="text" id="nome" name="nome">
-    <label for="">Data de Nascimento</label>
-    <input type="date" id="dataNascimento" name="dataNascimento" max="<?php echo date('Y-m-d', strtotime('-18 year')); ?>">
-    <label for="">E-mail</label>
-    <input type="email" id="emailUsuario" name="emailUsuario">
-    <label for="">Senha</label>
-    <input type="password" id="senhaUsuario" name="senhaUsuario">
-    <label for="">Numero de Brassagens</label>
-    <input type="number" id="brassagenscont" name="brassagenscont">
-    <label for="">Quant de Cursos</label>
-    <input type="number" id="cursoscerv" name="cursoscerv">
-    <button type="submit" id="cadastrar">Cadastrar</button>
-    </form>
+    <div class="container">
+    <h2>Adicionar Usuário</h2>
+        <form action="../Controller/controller.php" method="POST">
+            <div class="form-group">
+                <label for="nome">Nome:</label>
+                <input type="text" class="form-control" id="nome" name="nome">
+            </div>
+            <div class="form-group">
+                <label for="dataNascimento">Data de Nascimento:</label>
+                <input type="date" class="form-control" id="dataNascimento" name="dataNascimento" max="<?php echo date('Y-m-d', strtotime('-18 year')); ?>">
+            </div>
+            <div class="form-group">
+                <label for="emailUsuario">E-mail:</label>
+                <input type="email" class="form-control" id="emailUsuario" name="emailUsuario">
+            </div>
+            <div class="form-group">
+                <label for="senhaUsuario">Senha:</label>
+                <input type="password" class="form-control" id="senhaUsuario" name="senhaUsuario">
+            </div>
+            <div class="form-group">
+                <label for="brassagenscont">Número de Brassagens:</label>
+                <input type="number" class="form-control" id="brassagenscont" name="brassagenscont">
+            </div>
+            <div class="form-group">
+                <label for="cursoscerv">Quantidade de Cursos Feitos:</label>
+                <input type="number" class="form-control" id="cursoscerv" name="cursoscerv">
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </form>
+    </div>
 </body>
 </html>
