fdd4e5b23ccc37a813a23c257b5628b4f557de71
cleiton-Beal <bealcleiton66@gmail.com>
2021-05-12 20:19:27 -0300

    dao

--- DAO/UsuarioDAO.php
+++ DAO/UsuarioDAO.php
@@ -24,39 +24,7 @@ class UsuarioDAO{
             echo $erros;
 
         }
-
-
-
-
     }
-    public function criar(){
-        $erros = array();
-
-    if(!UsuarioValidate::validaEmail($_POST['emailUsuario'])){
-        $erros[] = 'E-mail Inválido!';
-    }
-    if(!UsuarioValidate::validaIdade($_POST['dataNascimento'])){
-        $erros[] = 'Idade abaixo do permitido!';
-    }
-
-    if (count($erros) == 0){
-        $usuario = new Usuario();
-
-        $usuario->nome = $_POST['nome'];
-        $usuario->dataNascimento = $_POST['dataNascimento'];
-        $usuario->emailUsuario = $_POST['emailUsuario'];
-        $usuario->senhaUsuario = $_POST['senhaUsuario'];
-        $usuario->brassagenscont = $_POST['brassagenscont'];
-        $usuario->cursoscerv = $_POST['cursoscerv'];
-
-        $userDao = new UsuarioDAO();
-        $userDao->create($usuario);
-    }
-    else {
-        echo "Ocorreram erros ao cadastrar um novo Usuário!";
-    }
-}
-    
 
     public function search(){
         try{
